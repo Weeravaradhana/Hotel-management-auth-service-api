08735f59a4c4b6d80cbb2ff9627da7cfd1ffe376
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-08-11 12:26:49 +0530

    Done implement system service impl method👍

--- src/main/java/com/cpd/hotel_system/auth_service_api/adviser/AppWideExceptionHandler.java
+++ src/main/java/com/cpd/hotel_system/auth_service_api/adviser/AppWideExceptionHandler.java
@@ -2,6 +2,8 @@ package com.cpd.hotel_system.auth_service_api.adviser;
 
 import com.cpd.hotel_system.auth_service_api.exception.BadRequestException;
 import com.cpd.hotel_system.auth_service_api.exception.DuplicateEntryException;
+import com.cpd.hotel_system.auth_service_api.exception.EntryNotFoundException;
+import com.cpd.hotel_system.auth_service_api.exception.UnAuthorizedException;
 import com.cpd.hotel_system.auth_service_api.util.StandardResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,5 +28,19 @@ public class AppWideExceptionHandler {
         );
     }
 
+    @ExceptionHandler(EntryNotFoundException.class)
+    public ResponseEntity<StandardResponseDto> handleEntryNotFoundException(EntryNotFoundException ex) {
+        return new ResponseEntity<StandardResponseDto>(
+                new StandardResponseDto(404,ex.getMessage(),ex),
+                HttpStatus.NOT_FOUND
+        );
+    }
 
+    @ExceptionHandler(UnAuthorizedException.class)
+    public ResponseEntity<StandardResponseDto> handleUnAuthorizedException(UnAuthorizedException ex) {
+        return new ResponseEntity<StandardResponseDto>(
+                new StandardResponseDto(401,ex.getMessage(),ex),
+                HttpStatus.UNAUTHORIZED
+        );
+    }
 }
